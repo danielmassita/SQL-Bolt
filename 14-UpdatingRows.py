bcfa74371ebc5a60b9486ada04ca7de1938a96a3
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-22 18:46:11 -0300

    Create 14-UpdatingRows.py

---
+++ 14-UpdatingRows.py
@@ -0,0 +1,6 @@
+# https://sqlbolt.com/lesson/updating_rows
+
+"""
+SQL Lesson 14: Updating rows
+In addition to adding new data, a common task is to update existing data, which can be done using an UPDATE statement. Similar to the INSERT statement, you have to specify exactly which table, columns, and rows to update. In addition, the data you are updating has to match the data type of the columns in the table schema.
+"""
