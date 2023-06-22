2592eb29e6c0b930c844260d2d116cbdc0ec6013
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-22 18:59:59 -0300

    Update 14-UpdatingRows.py

--- 14-UpdatingRows.py
+++ 14-UpdatingRows.py
@@ -4,3 +4,12 @@
 SQL Lesson 14: Updating rows
 In addition to adding new data, a common task is to update existing data, which can be done using an UPDATE statement. Similar to the INSERT statement, you have to specify exactly which table, columns, and rows to update. In addition, the data you are updating has to match the data type of the columns in the table schema.
 """
+# Update statement with values
+UPDATE mytable
+SET column = value_or_expr, 
+    other_column = another_value_or_expr, 
+    …
+WHERE condition;
+"""
+
+"""
