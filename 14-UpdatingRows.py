49ed70a652bdb04069b5ed31d8162c093fe8a8f9
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-22 19:10:14 -0300

    Update 14-UpdatingRows.py

--- 14-UpdatingRows.py
+++ 14-UpdatingRows.py
@@ -43,3 +43,26 @@ The director for A Bug's Life is incorrect, it was actually directed by John Las
 The year that Toy Story 2 was released is incorrect, it was actually released in 1999
 Both the title and director for Toy Story 8 is incorrect! The title should be "Toy Story 3" and it was directed by Lee Unkrich
 """
+SELECT * FROM movies;
+
+SELECT Title, Director FROM Movies
+WHERE Title = "A Bug's Life"
+# Then...
+UPDATE Movies
+SET Director = 'John Lasseter' 
+WHERE Id = '2';
+
+SELECT Id, Title, Year FROM movies
+WHERE Title LIKE "%toy story%";
+# Then... Let's change BY ID! 
+UPDATE Movies
+SET Year = '1999' 
+WHERE Id = '3';
+
+SELECT Id, Title, Director FROM movies
+WHERE Title LIKE "%toy story%";
+# Then...
+
+UPDATE Movies
+SET Director = 'Lee Unkrich', Title = 'Toy Story 3'
+WHERE Id = '11';
