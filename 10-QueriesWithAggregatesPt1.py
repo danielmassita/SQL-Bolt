cf259dbab77f0163a30537fa2bc8a5edad6549ae
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-22 01:40:44 -0300

    Create 10-QueriesWithAggregatesPt1.py

---
+++ 10-QueriesWithAggregatesPt1.py
@@ -0,0 +1,5 @@
+# https://sqlbolt.com/lesson/select_queries_with_aggregates
+
+"""
+In addition to the simple expressions that we introduced last lesson, SQL also supports the use of aggregate expressions (or functions) that allow you to summarize information about a group of rows of data. With the Pixar database that you've been using, aggregate functions can be used to answer questions like, "How many movies has Pixar produced?", or "What is the highest grossing Pixar film each year?".
+"""
