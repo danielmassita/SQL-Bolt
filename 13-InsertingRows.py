12d0132c2f02528ea7a2c48f35bd03da3d0abdcd
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-22 18:42:15 -0300

    Update 13-InsertingRows.py

--- 13-InsertingRows.py
+++ 13-InsertingRows.py
@@ -21,5 +21,56 @@ VALUES (value_or_expr, another_value_or_expr, …),
        (value_or_expr_2, another_value_or_expr_2, …),
        …;
 """
-
+In some cases, if you have incomplete data and the table contains columns that support default values, you can insert rows with only the columns of data you have by specifying them explicitly.
 """
+# Insert statement with specific columns
+INSERT INTO mytable
+(column, another_column, …)
+VALUES (value_or_expr, another_value_or_expr, …),
+      (value_or_expr_2, another_value_or_expr_2, …),
+      …;
+"""
+In these cases, the number of values need to match the number of columns specified. Despite this being a more verbose statement to write, inserting values this way has the benefit of being forward compatible. For example, if you add a new column to the table with a default value, no hardcoded INSERT statements will have to change as a result to accommodate that change.
+
+In addition, you can use mathematical and string expressions with the values that you are inserting.
+This can be useful to ensure that all data inserted is formatted a certain way.
+"""
+# Example Insert statement with expressions
+INSERT INTO boxoffice
+(movie_id, rating, sales_in_millions)
+VALUES (1, 9.9, 283742034 / 1000000);
+"""
+Exercise
+In this exercise, we are going to play studio executive and add a few movies to the Movies to our portfolio. In this table, the Id is an auto-incrementing integer, so you can try inserting a row with only the other columns defined.
+
+Since the following lessons will modify the database, you'll have to manually run each query once they are ready to go.
+
+Exercise 13 — Tasks
+Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. Add the record to the BoxOffice table.
+
+Table: Movies (Read-Only)
+Id	Title	Director	Year	Length_minutes
+1	Toy Story	John Lasseter	1995	81
+2	A Bug's Life	John Lasseter	1998	95
+3	Toy Story 2	John Lasseter	1999	93
+
+Table: Boxoffice (Read-Only)
+Movie_id	Rating	Domestic_sales	International_sales
+3	7.9	245852179	239163000
+1	8.3	191796233	170162503
+2	7.2	162798565	200600000
+"""
+SELECT * FROM movies;
+# Then...
+INSERT INTO Movies
+    (Title, Director, Year, Length_minutes) 
+VALUES 
+    ('Toy Story 4', 'Josh Cooley', '2019', '100');
+
+SELECT * FROM Boxoffice;
+# Then...
+INSERT INTO Boxoffice
+    (Movie_id, Rating, Domestic_sales, International_sales)
+VALUES
+    ('15', '8.7', '340000000', '270000000');
