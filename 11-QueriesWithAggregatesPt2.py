d76399d7edab36534c0937ebc1a7f5b16969fbb7
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-22 01:57:55 -0300

    Update 11-QueriesWithAggregatesPt2.py

--- 11-QueriesWithAggregatesPt2.py
+++ 11-QueriesWithAggregatesPt2.py
@@ -1,5 +1,16 @@
 # https://sqlbolt.com/lesson/select_queries_with_aggregates_pt_2
 
 """
+Our queries are getting fairly complex, but we have nearly introduced all the important parts of a SELECT query. One thing that you might have noticed is that if the GROUP BY clause is executed after the WHERE clause (which filters the rows which are to be grouped), then how exactly do we filter the grouped rows?
+
+Luckily, SQL allows us to do this by adding an additional HAVING clause which is used specifically with the GROUP BY clause to allow us to filter grouped rows from the result set.
+"""
+# Select query with HAVING constraint
+SELECT group_by_column, AGG_FUNC(column_expression) AS aggregate_result_alias, …
+FROM mytable
+WHERE condition
+GROUP BY column
+HAVING group_condition;
+"""
 
 """
