46ea7b6a25a0763e768793272472d995f5ecee91
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-22 01:59:13 -0300

    Update 11-QueriesWithAggregatesPt2.py

--- 11-QueriesWithAggregatesPt2.py
+++ 11-QueriesWithAggregatesPt2.py
@@ -12,5 +12,32 @@ WHERE condition
 GROUP BY column
 HAVING group_condition;
 """
+The HAVING clause constraints are written the same way as the WHERE clause constraints, and are applied to the grouped rows. With our examples, this might not seem like a particularly useful construct, but if you imagine data with millions of rows with different properties, being able to apply additional constraints is often necessary to quickly make sense of the data.
 
+Did you know?
+If you aren't using the `GROUP BY` clause, a simple `WHERE` clause will suffice.
+
+Exercise
+For this exercise, you are going to dive deeper into Employee data at the film studio. Think about the different clauses you want to apply for each task.
+
+Table: Employees
+Role	Name	Building	Years_employed
+Engineer	Becky A.	1e	4
+Engineer	Dan B.	1e	2
+Engineer	Sharon F.	1e	6
+Engineer	Dan M.	1e	4
+Engineer	Malcom S.	1e	1
+Artist	Tylar S.	2w	2
+Artist	Sherman D.	2w	8
+Artist	Jakob J.	2w	6
+Artist	Lillia A.	2w	7
+Artist	Brandon J.	2w	7
+Manager	Scott K.	1e	9
+Manager	Shirlee M.	1e	3
+Manager	Daria O.	2w	6
+
+Exercise 11 — Tasks
+Find the number of Artists in the studio (without a HAVING clause)
+Find the number of Employees of each role in the studio
+Find the total number of years employed by all Engineers
 """
