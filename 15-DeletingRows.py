ead58233b3b82a77716f070e0381d66228ac6fa8
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-23 12:58:06 -0300

    Update 15-DeletingRows.py

--- 15-DeletingRows.py
+++ 15-DeletingRows.py
@@ -12,4 +12,30 @@ If you decide to leave out the WHERE constraint, then all rows are removed, whic
 
 Taking extra care
 Like the UPDATE statement from last lesson, it's recommended that you run the constraint in a SELECT query first to ensure that you are removing the right rows. Without a proper backup or test database, it is downright easy to irrevocably remove data, so always read your DELETE statements twice and execute once.
+
+Exercise
+The database needs to be cleaned up a little bit, so try and delete a few rows in the tasks below.
+
+Table: Movies
+Id	Title	Director	Year	Length_minutes
+1	Toy Story	John Lasseter	1995	81
+2	A Bug's Life	John Lasseter	1998	95
+3	Toy Story 2	John Lasseter	1999	93
+4	Monsters, Inc.	Pete Docter	2001	92
+5	Finding Nemo	Andrew Stanton	2003	107
+6	The Incredibles	Brad Bird	2004	116
+7	Cars	John Lasseter	2006	117
+8	Ratatouille	Brad Bird	2007	115
+9	WALL-E	Andrew Stanton	2008	104
+10	Up	Pete Docter	2009	101
+11	Toy Story 3	Lee Unkrich	2010	103
+12	Cars 2	John Lasseter	2011	120
+13	Brave	Brenda Chapman	2012	102
+14	Monsters University	Dan Scanlon	2013	110
+
+Exercise 15 — Tasks
+This database is getting too big, lets remove all movies that were released before 2005.
+Andrew Stanton has also left the studio, so please remove all movies directed by him.
+"""
+
 """
